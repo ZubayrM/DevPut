e767555782d7c492edb04160f55832467679af0c
zubayr_@live.com <reflenie1roliK>
2020-05-17 22:58:48 +0300

    added security

---
+++ src/main/java/object/config/SecurityConfigurer.java
@@ -0,0 +1,50 @@
+package object.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    private static String path = "/api/post";
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(
+                        "/**",
+                        path + "/",
+                        path + "/search",
+                        path + "/byDate",
+                        path + "/byTag")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage(path + "login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .authoritiesByUsernameQuery("SELECT email, password FROM Users WHERE email=?");
+    }
+}
