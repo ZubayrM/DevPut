f973fa0681f003e90e2c6b27777ad5b8c8e04af8
zubayr_@live.com <reflenie1roliK>
2020-07-03 22:56:10 +0300

    captcha

--- src/main/java/object/services/CaptchaCodesService.java
+++ src/main/java/object/services/CaptchaCodesService.java
@@ -1,6 +1,7 @@
 package object.services;
 
 import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 import object.dto.response.CaptchaDto;
 import object.model.CaptchaCodes;
 import object.repositories.CaptchaCodesRepository;
@@ -10,15 +11,13 @@ import org.springframework.stereotype.Service;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.util.Base64;
 import java.util.Date;
 import java.util.UUID;
 
 @Service
+@Log4j2
 public class CaptchaCodesService {
 
     @Autowired
@@ -31,52 +30,73 @@ public class CaptchaCodesService {
 
         BufferedImage image = createBufferedImage(code);
 
-        CaptchaCodes captcha = createCaptcha(code, image);
+        String path = saveImage(image);
+        log.info(path);
 
-        File file = saveImage(captcha);
+        Thread.sleep(1000);
+        CaptchaCodes captcha = createCaptcha(code, path);
 
-        String url = createPathToImage(file);
 
         return CaptchaDto.builder()
-                .image(url)
+                .image(path)
                 .secret(code)
                 .build();
     }
 
-    private String createPathToImage(File file) {
+    private String getPathToImage(File file) {
         int index = file.getAbsolutePath().indexOf("img");
         return file.getAbsolutePath().substring(index-1);
     }
 
-    private File saveImage(CaptchaCodes captcha) throws IOException {
-        File file = new File("D:\\Диплом\\DevPut\\src\\main\\resources/static/img/photo.png");
-        byte[] img = Base64.getDecoder().decode(captcha.getCode());
-        BufferedImage bi =  ImageIO.read(new ByteArrayInputStream(img));
-        ImageIO.write(bi, "png", file);
 
-        return file;
+    private String saveImage(BufferedImage img) throws IOException {
+        String path = UUID.randomUUID().toString();
+        File dir = new File("D:\\Диплом\\DevPut\\src\\main\\resources/static/img/captcha/");
+        dir.mkdir();
+        File file = new File("D:\\Диплом\\DevPut\\src\\main\\resources/static/img/captcha/" + path  +".png");
+        ImageIO.write(img, "png", file);
+        return getPathToImage(file);
     }
 
-    @SneakyThrows
-    private CaptchaCodes createCaptcha(String code, BufferedImage image) {
 
-        String imgBase64 = getImgBase64(image);
+//    @Deprecated
+//    private File saveImage(CaptchaCodes captcha) throws IOException {
+//        File file = new File("D:\\Диплом\\DevPut\\src\\main\\resources/static/img/captcha.png");
+//        byte[] img = Base64.getDecoder().decode(captcha.getCode());
+//        BufferedImage bi =  ImageIO.read(new ByteArrayInputStream(img));
+//        ImageIO.write(bi, "png", file);
+//        return file;
+//    }
 
+//    @Deprecated
+//    @SneakyThrows
+//    private CaptchaCodes createCaptcha(String code, BufferedImage image) {
+//
+//        String imgBase64 = getImgBase64(image);
+//
+//        CaptchaCodes captchaCodes = new CaptchaCodes();
+//        captchaCodes.setTime(new Date());
+//        captchaCodes.setCode(imgBase64);
+//        captchaCodes.setSecretCode(code);
+//        return captchaCodesRepository.save(captchaCodes);
+//    }
+
+    private CaptchaCodes createCaptcha(String code, String file) {
         CaptchaCodes captchaCodes = new CaptchaCodes();
         captchaCodes.setTime(new Date());
-        captchaCodes.setCode(imgBase64);
+        captchaCodes.setCode(file);
         captchaCodes.setSecretCode(code);
         return captchaCodesRepository.save(captchaCodes);
     }
 
-    private String getImgBase64(BufferedImage image) throws IOException {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        ImageIO.write(image, "png", byteArrayOutputStream);
-        byteArrayOutputStream.flush();
-        byte[] bytes = byteArrayOutputStream.toByteArray();
-        byteArrayOutputStream.close();
-        return Base64.getEncoder().encodeToString(bytes);
-    }
+//    private String getImgBase64(BufferedImage image) throws IOException {
+//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//        ImageIO.write(image, "png", byteArrayOutputStream);
+//        byteArrayOutputStream.flush();
+//        byte[] bytes = byteArrayOutputStream.toByteArray();
+//        byteArrayOutputStream.close();
+//        return Base64.getEncoder().encodeToString(bytes);
+//    }
 
     private BufferedImage createBufferedImage(String code) {
         BufferedImage image = new BufferedImage(100, 40, BufferedImage.TYPE_3BYTE_BGR);
